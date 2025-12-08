5b9e884c053198b8d26fc103b41d2a85837c9ceb
TendryOne <tendryone@hotmail.com>
2025-12-08 23:51:47 +0300

    fix: improve MongoDB setup and health checks in GitHub Actions

--- database/index.js
+++ database/index.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
 const { mongoURI } = require("../config");
 
-mongoose
-  .connect(mongoURI)
-  .then(() => console.log("Connected to MongoDB "))
-  .catch((err) => console.error("Could not connect to MongoDB...", err));
+if (process.env.NODE_ENV !== "test") {
+  mongoose
+    .connect(mongoURI)
+    .then(() => console.log("Connected to MongoDB "))
+    .catch((err) => console.error("Could not connect to MongoDB...", err));
+}
 
 module.exports = mongoose;
